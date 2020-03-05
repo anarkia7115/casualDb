cc4e9a7018f3cdc62e8b48714f3edb8300e992a1
Gao JiaXiang <gaojxcs@gmail.com>
2020-03-06 07:05:25 +0800

    clean parser

--- main/parser.h
+++ main/parser.h
@@ -2,7 +2,7 @@
 #define PARSER_H_
 #include "statement.h"
 #include <sstream>
-PrepareResult ParseInsert(string query, Row &row_to_insert) {
+PrepareResult ParseInsert(std::string query, Row &row_to_insert) {
     /*
     id int
     url string
@@ -11,15 +11,14 @@ PrepareResult ParseInsert(string query, Row &row_to_insert) {
     > insert 1 cnn.com/one_news.html i_m_a_news
     */
 
-    istringstream query_stream(query);
-    string query_keyword;
-    query_stream >> query_keyword >> row_to_insert.id >> row_to_insert.url 
+    std::istringstream query_stream(query);
+    std::string query_keyword;
+    query_stream >> query_keyword 
+        >> row_to_insert.id 
+        >> row_to_insert.url 
         >> row_to_insert.title;
 
-    if (query_keyword != "insert") {
-        return kPrepareUnrecognizedStatement;
-    } else {
-        return kPrepareSuccess;
-    }
+    return kPrepareSuccess;
 }
+
 #endif // PARSER_H_
