53a991d3f1cb8ba1c14a76c18f5c431e142eaa71
Gao JiaXiang <gaojxcs@gmail.com>
2020-03-12 21:52:15 +0800

    update test

--- test/repl-test.py
+++ test/repl-test.py
@@ -2,25 +2,35 @@ import textwrap
 import pytest
 import subprocess
 
-def test_repl_insert_full():
+
+def exec_sql(input_sql: list):
     p = subprocess.Popen("./casualdb", 
         stdin=subprocess.PIPE, 
         stdout=subprocess.PIPE)
+
+    output = p.communicate("\n".join(input_sql).encode())
+    return output[0].strip().split(b"\n")
+
+
+def test_repl_insert_full():
     do_sql = ["insert {i} www.{i}.com News[{i}]".format(i=i) for i in range(1000)]
-    batch_sql = "\n".join(do_sql) + "\n.exit"
-    output = p.communicate(batch_sql.encode())
-    assert output[0].split(b"\n")[-3] == b"db > Error: Table full."
+    do_sql.append(".exit")
+    output = exec_sql(do_sql)
+
+    assert output[-3] == b"db > Error: Table full."
     
 
 def test_repl_insert_one():
-    p = subprocess.Popen("./casualdb", 
-        stdin=subprocess.PIPE, 
-        stdout=subprocess.PIPE)
-    sql = b"insert 1 www.1.com News[1]\nselect\n.exit"
-    output = p.communicate(sql)
-    assert output[0].strip().split(b"\n") == [
-        b"db > Executed!", 
-        b"db > (1, www.1.com, News[1])", 
-        b"Executed!", 
-        b"db > Bye!", 
+    sql = [
+        "insert 1 www.1.com News[1]", 
+        "select", 
+        ".exit"
     ]
+    output = exec_sql(sql)
+    expected_output = [
+        "db > Executed!", 
+        "db > (1, www.1.com, News[1])", 
+        "Executed!", 
+        "db > Bye!", 
+    ]
+    assert output == [row.encode() for row in expected_output]
