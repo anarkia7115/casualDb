f9933edb9e015139e167d0b85c4ebc8204fbe3c1
Gao JiaXiang <gaojxcs@gmail.com>
2020-03-13 07:45:50 +0800

    add pager

--- main/table.cc
+++ main/table.cc
@@ -1,14 +1,46 @@
 #ifndef TABLE_CC_
 #define TABLE_CC_
 
-#include "table.h"
+#include <iostream>
 #include <cstdlib>
+#include "fcntl.h"
+#include "unistd.h"
 
-Table::Table() {
-    this->num_rows_ = 0;
-    for (uint32_t i=0; i < kTableMaxPages; i++) {
+#include "table.h"
+#include "row.h"
+
+Pager::Pager(std::string db_file) {
+    int fd = open(
+        db_file.c_str(), 
+        O_RDWR |
+            O_CREAT, 
+        S_IWUSR |
+            S_IRUSR
+    );
+
+    if (fd == -1) {
+        std::cout << "Unable to open file"
+            << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    off_t file_length = lseek(fd, 0, SEEK_END);
+
+    this->file_descriptor = fd;
+    this->file_length = file_length;
+
+    for (uint32_t i=0; i< kTableMaxPages; i++) {
         this->pages_[i] = NULL;
     }
+}
+
+Table::Table(std::string db_file) {
+    Pager pager(db_file);
+    this->num_rows_ = pager.file_length / Row::kRowSize;
+}
+
+Table::Table() {
+    Table(".casual_db");
 }
 
 Table::~Table() {
@@ -18,13 +50,32 @@ Table::~Table() {
 }
 
 void* 
+Pager::GetPage(uint32_t page_num) {
+    if (page_num > kTableMaxPages) {
+        std::cout << "Tried to fetch page number out of max" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (this->pages_[page_num] == NULL) {
+        void* page = malloc(kPageSize);
+        uint32_t num_pages = file_length / kPageSize;
+
+        // TODO: save a partial page
+        if (file_length % kPageSize) {
+            num_pages += 1;
+        }
+
+        // read if not new
+        if (page_num <= num_pages)
+
+    }
+
+}
+
+void* 
 Table::RowSlot(uint32_t row_num) {
     uint32_t page_num = row_num / kRowsPerPage;
-    void* page = this->pages_[page_num];
-    if (page == NULL) {
-        // Allocate memory only when we try to access page
-        page = this->pages_[page_num] = malloc(kPageSize);
-    }
+    void* page = this->pager_->GetPage(page_num);
     uint32_t row_offset = row_num % kRowsPerPage;
     uint32_t byte_offset = row_offset * Row::kRowSize;
     return page + byte_offset;
--- main/table.h
+++ main/table.h
@@ -1,6 +1,7 @@
 #ifndef TABLE_H_
 #define TABLE_H_
 
+#include <string>
 #include <cstdint>
 #include "row.h"
 #include "state.h"
@@ -10,12 +11,23 @@ const uint32_t kPageSize = 4096;
 const uint32_t kRowsPerPage = kPageSize / Row::kRowSize;
 const uint32_t kTableMaxRows = kRowsPerPage * kTableMaxPages;
 
+class Pager {
+  int file_descriptor;
+  void* pages_[kTableMaxPages];
+public:
+  uint32_t file_length;
+  Pager(std::string db_file);
+  void* GetPage(std::uint32_t page_number);
+  ~Pager();
+};
+
 class Table {
     uint32_t num_rows_;
-    void* pages_[kTableMaxPages];
+    Pager* pager_;
     void* RowSlot(std::uint32_t row_num);
 
   public:
+    Table(std::string db_file);
     Table();
     ~Table();
     ExecuteResult Insert(Row row);
