bc41a00996e9da08b35c6228a4695b99fe28752a
Gao JiaXiang <gaojxcs@gmail.com>
2020-03-12 20:25:44 +0800

    add time function

--- main/utils.h
+++ main/utils.h
@@ -1,10 +1,16 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 #include <string>
+#include <ctime>
 
 bool StartsWith(std::string one_string, std::string another_string) {
     // one_string.substr(0, another_string.length)
     return one_string.compare(0, another_string.size(), another_string) == 0;
+}
+
+std::string NowTime() {
+    std::time_t now = std::time(0);
+    return std::ctime(&now);
 }
 
 #endif // UTILS_H_
